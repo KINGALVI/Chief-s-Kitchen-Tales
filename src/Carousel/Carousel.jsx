d4ffd1b9df69a9ce9d24968e95b6c4d674c08a79
KINGALVI <alvialmahamud@gmail.com>
2025-04-11 05:38:21 +0200

    edited some code

--- src/Carousel/Carousel.jsx
+++ src/Carousel/Carousel.jsx
@@ -12,13 +12,14 @@ function Carousel() {
       <Card.ImgOverlay className='d-flex justify-content-center align-items-center'>
         <div>
           <div className='text-center pb-3'>
-            <Card.Title><h1><b>Discover an exceptional cooking class tailored for you!</b></h1></Card.Title>
+            <Card.Title><h1><b>Discover an exceptional cooking <br /> class tailored for you!</b></h1></Card.Title>
+            <br />
             <Card.Text>
               Step into the world of culinary excellence! Whether you're a seasoned chef or a passionate food enthusiast,
-              embark on a journey filled <br /> with mouthwatering recipes, expert techniques, and the secrets to creating unforgettable dishes.
-              Let your kitchen tales unfold here!
+              embark on a journey filled with <br /> mouthwatering recipes, expert techniques, and the secrets to creating unforgettable dishes. Let your kitchen tales unfold here!
             </Card.Text>
           </div>
+          <br />
           <div className='d-flex justify-content-center'>
             <Button variant="success text-dark m-1"><b>Explore Now</b></Button>
             <Button variant="outline-light m-1">Our Feedback</Button>
