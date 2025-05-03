8d821d644fb7eebc09b7064f9f469466d0f8b650
KING ALVI <94359754+KINGALVI@users.noreply.github.com>
2025-05-03 23:26:19 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-// Import React library and ReactDOM for rendering the application
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Import the main App component
 import App from './App';
 
-// Import the global styles (if applicable)
 import './index.css';
 
-// Create the root element and render the App component inside it
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
